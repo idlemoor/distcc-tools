b87eef4ff9e9af7a6ddbfb9a6afc7dc03e98bcf5
David Spencer <baildon.research@googlemail.com>
2015-09-21 10:41:09 +0100

    distcc-masq-client: force GCCVERSION onto gcc invocations.

--- distcc-masq-client/masq-gcc.sh
+++ distcc-masq-client/masq-gcc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 [ -z "$DISTCC_HOSTS" ] && exec /usr/bin/%ARCH%-slackware-linux%SLKABI%-gcc "$@"
-exec /usr/bin/distcc /usr/bin/%ARCH%-slackware-linux%SLKABI%-gcc "$@"
+exec /usr/bin/distcc /usr/bin/%ARCH%-slackware-linux%SLKABI%-gcc-%GCCVERSION% "$@"
--- distcc-masq-client/masq-gfortran.sh
+++ distcc-masq-client/masq-gfortran.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 [ -z "$DISTCC_HOSTS" ] && exec /usr/bin/%ARCH%-slackware-linux%SLKABI%-gfortran "$@"
-exec /usr/bin/distcc /usr/bin/%ARCH%-slackware-linux%SLKABI%-gfortran "$@"
+exec /usr/bin/distcc /usr/bin/%ARCH%-slackware-linux%SLKABI%-gfortran-%GCCVERSION% "$@"
