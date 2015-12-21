6a70588cd34ce7b85dbcef011396cdc75423df65
David Spencer <baildon.research@googlemail.com>
2015-12-21 21:24:47 +0000

    distcc-masq-client: Fix calling ccache and distcc explictly.
    For example 'ccache -s' or 'distcc --version'.

--- distcc-masq-client/masq-prevent-recursion.sh
+++ distcc-masq-client/masq-prevent-recursion.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-if [ "$1" == 'distcc' ] || [ "$1" == '/usr/bin/distcc' ] || [ "$1" == 'ccache' ] || [ "$1" == '/usr/bin/ccache' ]; then shift; fi
+if [ "$1" == 'distcc' ] || [ "$1" == '/usr/bin/distcc' ] || [ "$1" == 'ccache' ] || [ "$1" == '/usr/bin/ccache' ]; then
+  shift
+else
+  case "$1" in
+    -*) exec /usr/bin/"$(basename "$0")" "$@" ;;
+     *) : ;;
+  esac
+fi
 exec "$@"
