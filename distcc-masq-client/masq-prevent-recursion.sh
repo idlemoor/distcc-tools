0c2fa97525454a74a94529b4b753086adcfc1e88
David Spencer <baildon.research@googlemail.com>
2017-05-28 23:56:31 +0100

    distcc-masq-client: Updated for 14.2 and -current 2017-05-22.

--- distcc-masq-client/masq-prevent-recursion.sh
+++ distcc-masq-client/masq-prevent-recursion.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "$1" == 'distcc' ] || [ "$1" == '/usr/bin/distcc' ] || [ "$1" == 'ccache' ] || [ "$1" == '/usr/bin/ccache' ]; then
+if [ "$1" = 'distcc' ] || [ "$1" = '/usr/bin/distcc' ] || [ "$1" = 'ccache' ] || [ "$1" = '/usr/bin/ccache' ]; then
   shift
 else
   case "$1" in
