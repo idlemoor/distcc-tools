e3f3766bb03b27bb00b3e0a05af63bebaeccb090
David Spencer <baildon.research@googlemail.com>
2017-09-19 16:36:14 +0100

    (all): Update for arm-current gnueabihf (thanks to yustin).

--- arm-x-toolchain/arm-gccwrapper.sh
+++ arm-x-toolchain/arm-gccwrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=%INSTLOC%/%ARCH%-slackware-linux/arm-slackware-linux-gnueabi/lib
-exec %INSTLOC%/bin/arm-slackware-linux-gnueabi-gcc "$@"
+export LD_LIBRARY_PATH=%INSTLOC%/%ARCH%-slackware-linux/arm-slackware-linux-%GNUEABI%/lib
+exec %INSTLOC%/bin/arm-slackware-linux-%GNUEABI%-gcc "$@"
